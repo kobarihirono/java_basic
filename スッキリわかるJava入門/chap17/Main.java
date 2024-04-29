d9a029d4574ebfa0f549f897c9b8173f1f7cbb5d
小針寛乃 <kobarihirono@kobarihiroshinonoMacBook-Pro.local>
2024-04-29 20:54:52 +0900

    [new]テストコミット

--- スッキリわかるJava入門/chap17/Main.java
+++ スッキリわかるJava入門/chap17/Main.java
@@ -1,13 +1,13 @@
 package スッキリわかるJava入門.chap17;
-import java.io.FileWriter;
-import java.io.IOException;
+// import java.io.FileWriter;
+// import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            FileWriter file = new FileWriter("data.txt");
-        } catch (IOException e) { // FileWriterのコンストラクタで例外が発生した場合、IOExceptionをキャッチする必要がある
-            System.out.println("エラーが発生しました");
-        }
+        // try {
+        //     FileWriter file = new FileWriter("data.txt");
+        // } catch (IOException e) { // FileWriterのコンストラクタで例外が発生した場合、IOExceptionをキャッチする必要がある
+        //     System.out.println("エラーが発生しました");
+        // }
     }
 }
---
+++ 黒本/Main.java
@@ -0,0 +1,16 @@
+package 黒本;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a++ + a + a-- - a-- + ++a;
+        // int c = a += 10;
+        // int num = -10;
+        
+        // System.out.println(a + c);
+        // System.out.println(10 * -num);
+
+        // int a = 100, b = 200;
+        // System.out.println(a + b);
+    }
+}
