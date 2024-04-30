01eab3ee016113c7b27060629c165204bd16ce7e
小針寛乃 <kobarihirono@kobarihiroshinonoMacBook-Pro.local>
2024-04-30 22:17:16 +0900

    [new]chap6

---
+++ 黒本/chap6/Main.java
@@ -0,0 +1,10 @@
+package 黒本.chap6;
+
+public class Main {
+    public static void main(String[] args) {
+        Sample s = new Sample();
+        String val = s.getValue("hello");
+        s.getValue();
+        System.out.println(val);
+    }
+}
---
+++ 黒本/chap6/Sample.java
@@ -0,0 +1,13 @@
+package 黒本.chap6;
+
+public class Sample {
+    private String value;
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+}
