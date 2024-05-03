a96843046c7ac489824e7f0279116c6b7c32f8f3
小針寛乃 <kobarihirono@kobarihiroshinonoMacBook-Pro.local>
2024-05-03 12:21:42 +0900

    [fix]エラーの解消

--- 黒本/chap6/Main.java
+++ 黒本/chap6/Main.java
@@ -3,8 +3,8 @@ package 黒本.chap6;
 public class Main {
     public static void main(String[] args) {
         Sample s = new Sample();
-        String val = s.getValue("hello");
+        // String val = s.getValue("hello");
         s.getValue();
-        System.out.println(val);
+        // System.out.println(val);
     }
 }
