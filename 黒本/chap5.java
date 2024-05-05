ed4fb66ea624691c5688493b3226d8154b67eaa5
小針寛乃 <kobarihirono@kobarihiroshinonoMacBook-Pro.local>
2024-05-05 19:38:55 +0900

    [new]chap5

--- 黒本/chap5.java
+++
@@ -1,9 +0,0 @@
-package 黒本;
-
-public class chap5 {
-    public static void main(String[] args) {
-        int[] array = new int[0];
-        System.out.println(array); // ハッシュコードが表示される
-        System.out.println(array[0]); // ArrayIndexOutOfBoundsException
-    }
-}
---
+++ 黒本/chap5/Item.java
@@ -0,0 +1,6 @@
+package 黒本.chap5;
+
+public class Item {
+    String name;
+    int price = 100;
+}
